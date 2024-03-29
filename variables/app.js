711c020c11d899b2be7367a75e63ca2c055b2ea9
maliha iqbal <malihaiqbal.241@gmail.com>
2024-03-29 19:35:50 +0500

    Add files via upload

---
+++ variables/app.js
@@ -0,0 +1,10 @@
+var myname = "Maliha iqbal";
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+myname = "Maliha";
+console.log(myname);
+var pi = 3.142;
+console.log(pi);
---
+++ variables/app.ts
@@ -0,0 +1,14 @@
+let myname = "Maliha iqbal"
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+
+myname = "Maliha"
+console.log(myname);
+
+
+
+const pi = 3.142
+console.log(pi);
