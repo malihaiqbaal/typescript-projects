59076c96885e0f27a7fedf9bb64a9f92d5a8d313
maliha iqbal <malihaiqbal.241@gmail.com>
2024-04-02 01:21:53 +0500

    Add files via upload

---
+++ Q3/app.js
@@ -0,0 +1,9 @@
+// for lower case//
+var myname = ("maliha iqbal");
+console.log(myname);
+//for uper case//
+myname = ("MALIHA IQBAL");
+console.log(myname);
+//for title case//
+myname = ("Maliha Iqbal");
+console.log(myname);
---
+++ Q3/app.ts
@@ -0,0 +1,11 @@
+// for lower case//
+let myname=("maliha iqbal");
+console.log(myname);
+
+//for uper case//
+myname=("MALIHA IQBAL");
+console.log(myname);
+
+//for title case//
+myname=("Maliha Iqbal");
+console.log(myname);
