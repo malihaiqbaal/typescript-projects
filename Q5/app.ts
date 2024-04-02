c9984819c45d44dc6800812b7c6ddd189c8806bf
maliha iqbal <malihaiqbal.241@gmail.com>
2024-04-03 00:38:57 +0500

    Add files via upload

---
+++ Q5/app.ts
@@ -0,0 +1,4 @@
+const famous_person = "Albert Einstein";
+const quote = "A person who never made a mistake never tried anything new.";
+const message = famous_person + " once said, " + '"' + quote + '"';
+console.log(message);
