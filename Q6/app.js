394e24d32667e1a5e78c1e0deec5c56ee577c5be
maliha iqbal <malihaiqbal.241@gmail.com>
2024-04-03 00:39:53 +0500

    Add files via upload

---
+++ Q6/app.js
@@ -0,0 +1,4 @@
+var name_with_whitespace = "\t\n   John Doe   \t\n";
+console.log("Name with whitespace:", name_with_whitespace);
+var name_stripped = name_with_whitespace.trim();
+console.log("Stripped name:", name_stripped);
