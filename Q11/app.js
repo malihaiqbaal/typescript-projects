0aa3a523efa621a6364967c16e75ab0d1c9c1bf0
maliha iqbal <malihaiqbal.241@gmail.com>
2024-04-09 02:09:34 +0500

    Add files via upload

---
+++ Q11/app.js
@@ -0,0 +1,4 @@
+var friendName = ["ALISHBA", "UROJ", "MANAHIL", "LAIBA", "MALIHA"];
+for (var i = 0; i < friendName.length; i++) {
+    console.log(friendName[i]);
+}
---
+++ Q11/app.ts
@@ -0,0 +1,5 @@
+let friendName = ["ALISHBA", "UROJ" , "MANAHIL" , "LAIBA" , "MALIHA"];
+
+for(let i=0 ; i<friendName.length ; i++){
+    console.log(friendName[i]);
+}
