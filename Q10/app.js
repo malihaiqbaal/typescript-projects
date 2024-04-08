92ab27aa1142947419aec4c2c0e85c41467fc6f4
maliha iqbal <malihaiqbal.241@gmail.com>
2024-04-09 02:09:00 +0500

    Add files via upload

---
+++ Q10/app.js
@@ -0,0 +1,4 @@
+// this line define my name//
+var myName = "Maliha Iqbal";
+// this line define current date//
+var currentDate = "5/5/2024";
---
+++ Q10/app.ts
@@ -0,0 +1,5 @@
+// this line define my name//
+let myName = "Maliha Iqbal";
+
+// this line define current date//
+let currentDate = "5/5/2024";
