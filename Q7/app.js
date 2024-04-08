9ed7c6f740731715efd1e8ed72d74f9bf265730a
maliha iqbal <malihaiqbal.241@gmail.com>
2024-04-09 02:06:49 +0500

    Add files via upload

---
+++ Q7/app.js
@@ -0,0 +1,12 @@
+var num1 = 5;
+var num2 = 3;
+console.log(num1 + num2);
+var num3 = 10;
+var num4 = 2;
+console.log(num3 - num4);
+var num5 = 4;
+var num6 = 2;
+console.log(num5 * num6);
+var num7 = 16;
+var num8 = 2;
+console.log(num7 / num8);
