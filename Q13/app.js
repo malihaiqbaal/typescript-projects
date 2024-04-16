b1eea330e86513256bc25b94e90e148536137711
maliha iqbal <malihaiqbal.241@gmail.com>
2024-04-17 01:43:18 +0500

    Add files via upload

---
+++ Q13/app.js
@@ -0,0 +1,6 @@
+// List of favorite modes of transportation
+var transportation = ['motorcycle', 'car', 'bicycle', 'scooter'];
+// Printing statements about the items in the list
+transportation.forEach(function (item) {
+    console.log("I would like to own a " + transportation[1]);
+});
---
+++ Q13/app.ts
@@ -0,0 +1,7 @@
+// List of favorite modes of transportation
+const transportation: string[] = ['motorcycle', 'car', 'bicycle', 'scooter'];
+
+// Printing statements about the items in the list
+transportation.forEach(item => {
+    console.log("I would like to own a "+ transportation[1])
+});
