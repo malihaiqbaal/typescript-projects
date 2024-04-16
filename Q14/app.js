eb59e30d434c34b7cf39c7f0193eb7b3caffe303
maliha iqbal <malihaiqbal.241@gmail.com>
2024-04-17 01:44:01 +0500

    Add files via upload

---
+++ Q14/app.js
@@ -0,0 +1,5 @@
+var friends = ["ALISHBA", "MANAHIL", "LAIBA", "UROJ", "MALIHA"];
+for (var i = 0; i < friends.length; i++) {
+    console.log(friends[i] + " - " + "i am inviting you at my house for dinner" + "<br>");
+}
+;
---
+++ Q14/app.ts
@@ -0,0 +1,6 @@
+let friends = ["ALISHBA", "MANAHIL", "LAIBA", "UROJ", "MALIHA"];
+
+
+   for( let i=0;i < friends.length; i++){
+        console.log(friends[i] + " - " + "i am inviting you at my house for dinner"+"<br>");
+   };
